21a2a6a3e6140ab4563c1647c38905d02a81ee8d
ashmop <40921672+ashmop@users.noreply.github.com>
2018-07-20 15:06:19 +0300

    добавляли классы и еще что-то

--- test_add_group.py
+++ test_add_group.py
@@ -20,6 +20,7 @@ class test_add_group(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, user, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(user)
@@ -33,6 +34,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -47,6 +49,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
@@ -56,21 +59,15 @@ class test_add_group(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group("sss", "aaa", "kkk"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.open_groups_page(wd)
         self.create_group(wd,  Group("", "", ""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def tearDown(self):
