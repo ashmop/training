7fecf1a2ecbe94ced60d2883c6f100d01efac509
ashmop <40921672+ashmop@users.noreply.github.com>
2018-08-25 19:49:15 +0300

    создана интеллектуальная фикстура

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,14 +3,9 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login("admin", "secret")
     app.group.create(Group("sss", "aaa", "kkk"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login("admin", "secret")
     app.group.create(Group("", "", ""))
-    app.session.logout()
-
 
